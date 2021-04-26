94df1b5dd8e58d15bfa5e3a7a132a3479c9ab0ba
Ivan ENa <ivan.ena@infowatch.com>
2021-04-26 15:12:57 +0300

    * portal: Добавил js код для скрытия редактирования;

--- public_html/modules/custom/form_exposed_views_custom/js/form_exposed_views_custom.js
+++ public_html/modules/custom/form_exposed_views_custom/js/form_exposed_views_custom.js
@@ -4,36 +4,30 @@
 
     Drupal.behaviors.form_utechki_daydzhesty = {
         attach: function (context, settings) {
-          $('input[name="publication"]').datepicker({
-            dateFormat: 'dd.mm.yy',
-            monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-            dayNamesMin : ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-            firstDay: 1
-          })
-
-          $('#views-exposed-form-all-materials-block-1').once('all-materials-active').each(function (i) {
-            $(this).find('[data-drupal-selector="edit-reset"]').click(function (e) {
-              e.preventDefault()
-              location.href = '/'
+            $('input[name="publication"]').datepicker({
+                dateFormat: 'dd.mm.yy',
+                monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+                firstDay: 1
             })
 
-            /*$('[data-drupal-selector="edit-submit-all-materials').click(function (e) {
-              var option_selected = $('[data-drupal-selector="edit-types"] option:selected').val()
+            $('#views-exposed-form-all-materials-block-1').once('all-materials-active').each(function (i) {
+                $(this).find('[data-drupal-selector="edit-reset"]').click(function (e) {
+                    e.preventDefault()
+                    location.href = '/'
+                })
+            })
 
-              if (location.pathname == '/node/14' && option_selected != 3) {
-                e.preventDefault()
-                location.href = '/?types='+option_selected
-              } else  if (option_selected == 3) {
-                e.preventDefault()
-                location.href = '/node/14'
-              }
-
-            })*/
-
-          })
+            $('.success-add').once('success-add-active').each(function (i) {
+                if ($('.approve-node-form .success-add.yes').length > 0) {
+                    $('nav.tabs').hide()
+                } else {
+                    $('nav.tabs').show()
+                }
+            })
 
 
 
         }
     }
-} (jQuery, Drupal, drupalSettings));
+}(jQuery, Drupal, drupalSettings));
